d29dca82193403091597236e574d67ba366be1d1
Rhuan Kowic <dev.rhuankowic@gmail.com>
2025-12-23 09:05:57 -0300

    feat: adiciona auth controller e cria rota de login.

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,47 @@
+import z from "zod";
+import { prisma } from "../lib/prisma";
+import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
+import { compare } from "bcryptjs";
+
+export class AuthController {
+  async authenticate(req: Request, res: Response) {
+    const authBody = z.object({
+      email: z.string().email(),
+      password: z.string(),
+    });
+
+    const { email, password } = authBody.parse(req.body);
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "Email ou senha incorretos." });
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Email ou senha incorretos." });
+    }
+
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) {
+      throw new Error("JWT_SECRET não definido no .env");
+    }
+
+    const token = sign({ id: user.id }, secret, { expiresIn: "7d" });
+
+    return res.json({
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    });
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { CategoryController } from "./controllers/CategoryController";
 import { TransactionController } from "./controllers/TransactionController";
 import { UserController } from "./controllers/UserController";
+import { AuthController } from "./controllers/AuthController";
 
 const router = Router();
 const categoryController = new CategoryController();
 const transactionController = new TransactionController();
 const userController = new UserController();
+const authController = new AuthController();
 
 // Rotas de Categorias
 router.post("/categories", categoryController.create);
@@ -19,5 +21,8 @@ router.delete("/transactions/:id", transactionController.delete);
 
 // Rotas de usuarios
 router.post("/users", userController.create);
+
+// Rota de Login
+router.post("/login", authController.authenticate);
 
 export { router };
